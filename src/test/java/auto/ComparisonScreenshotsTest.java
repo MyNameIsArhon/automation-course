a945af0ff48d4ab9dd43a8d27c3da6dc513d1764
Oleg <denisenko.oleg377@gmail.com>
2025-09-02 13:53:43 +0300

    Практическое задание 5

--- src/test/java/auto/ComparisonScreenshotsTest.java
+++ src/test/java/auto/ComparisonScreenshotsTest.java
@@ -22,7 +22,7 @@ public class ComparisonScreenshotsTest {
     @BeforeEach
     void setup() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
         context = browser.newContext();
         page = context.newPage();
     }
