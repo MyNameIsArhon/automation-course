cf8dba41c1bb983f5becd5b98fb21514466c4792
Oleg <denisenko.oleg377@gmail.com>
2025-09-10 20:17:05 +0300

    Практическое задание 14

---
+++ src/test/java/auto/MobileDragAndDropTest.java
@@ -0,0 +1,57 @@
+package auto;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class MobileDragAndDropTest {
+    Playwright playwright;
+    Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeEach
+    void setup() {
+        playwright = Playwright.create();
+
+        // Ручная настройка параметров Samsung Galaxy S22 Ultra
+        Browser.NewContextOptions deviceOptions = new Browser.NewContextOptions()
+                .setUserAgent("Mozilla/5.0 (Linux; Android 12; SM-S908B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Mobile Safari/537.36")
+                .setViewportSize(384, 873)  // Разрешение экрана
+                .setDeviceScaleFactor(3.5)
+                .setIsMobile(true)
+                .setHasTouch(true);
+
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext(deviceOptions);
+        page = context.newPage();
+    }
+
+    @Test
+    void testDragAndDropMobile() {
+        page.navigate("https://the-internet.herokuapp.com/drag_and_drop");
+
+        Locator columnA = page.locator("#column-a");
+        Locator columnB = page.locator("#column-b");
+
+        assertThat(columnA).hasText("A");
+        assertThat(columnB).hasText("B");
+
+        columnA.dragTo(columnB);
+
+        page.waitForTimeout(1000);
+        assertThat(columnA).hasText("B");
+        assertThat(columnB).hasText("A");
+    }
+
+    @AfterEach
+    void tearDown() {
+        context.close();
+        browser.close();
+        playwright.close();
+    }
+
+}
