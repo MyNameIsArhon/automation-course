dc42d4ff72e2b132663c059184f02611902cfa55
Oleg <denisenko.oleg377@gmail.com>
2025-09-11 18:40:38 +0300

    Практическое задание 17

---
+++ src/test/java/auto/HoverTest.java
@@ -0,0 +1,64 @@
+package auto;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.WaitUntilState;
+import org.junit.jupiter.api.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HoverTest {
+
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void setupClass() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @BeforeEach
+    void setUp() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @Test
+    void testHoverProfiles() {
+        page.navigate("https://the-internet.herokuapp.com/hovers",
+                new Page.NavigateOptions().setWaitUntil(WaitUntilState.DOMCONTENTLOADED));
+
+        Locator figures = page.locator(".figure");
+        int count = figures.count();
+
+        for (int i = 0; i < count; i++) {
+            Locator figure = figures.nth(i);
+            figure.hover();
+
+            Locator profileLink = figure.locator("text=View profile");
+            assertThat(profileLink).isVisible();
+            assertThat(profileLink).hasText("View profile");
+
+            profileLink.click();
+            assertThat(page).hasURL("https://the-internet.herokuapp.com/users/%s".formatted(i+1));
+
+            page.goBack();
+        }
+    }
+
+    @AfterAll
+    static void teardownClass() {
+        browser.close();
+        playwright.close();
+    }
+
+
+    @AfterEach
+    void teardow() {
+        context.close();
+    }
+
+}
