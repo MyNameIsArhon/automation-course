30ba178850f6a2122bc9b2abdef8882b28ffb7c8
Oleg <denisenko.oleg377@gmail.com>
2025-09-16 23:30:19 +0300

    Практическое задание 20

---
+++ src/test/java/auto/FakerGenerTest.java
@@ -0,0 +1,35 @@
+package auto;
+
+import com.github.javafaker.Faker;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.WaitUntilState;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class FakerGenerTest {
+
+    Faker faker = new Faker();
+
+    @Test
+    void testFakerGenerate() {
+        try (Playwright playwright = Playwright.create();
+             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+             BrowserContext context = browser.newContext();
+             Page page = context.newPage()) {
+
+            String expectedName = faker.name().fullName();
+            Locator actualName = page.locator(".large-10.columns");
+
+            page.route("**/dynamic_content", route -> {
+                route.fulfill(new Route.FulfillOptions()
+                        .setBody("<div class='large-10 columns'>" + expectedName + "</div>"));
+                    });
+
+            page.navigate("https://the-internet.herokuapp.com/dynamic_content");
+            assertThat(actualName).hasText(expectedName);
+        }
+    }
+}
