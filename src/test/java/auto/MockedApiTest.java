1336a90c54f404edf4597ad756a653a61676a700
Oleg <denisenko.oleg377@gmail.com>
2025-09-29 19:24:51 +0300

    Практическое задание 28

--- src/test/java/auto/MockedApiTest.java
+++ src/test/java/auto/MockedApiTest.java
@@ -19,9 +19,7 @@ public class MockedApiTest {
     @BeforeAll
     static void setUpClass() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
-        );
+        browser = playwright.chromium().launch();
 
         apiService = mock(ApiButtonsService.class);
         when(apiService.getCountButtons("row")).thenReturn("{\"count\": \"5\"}");
